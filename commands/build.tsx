9e44c0d283762274e339b6508a71362acfaaefb8
Adam Bergman <adam@fransvilhelm.com>
2020-09-22 16:38:06 +0200

    Setup pastel and ink

---
+++ commands/build.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Box, Text } from 'ink';
+
+interface Props {}
+
+const Build: React.FC<Props> = () => {
+  return (
+    <Box>
+      <Text>build</Text>
+    </Box>
+  );
+};
+
+Build.propTypes = {};
+
+export default Build;
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  testEnvironment: 'node',
 };
